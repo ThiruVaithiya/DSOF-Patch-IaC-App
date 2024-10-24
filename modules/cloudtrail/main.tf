1eb1d13c67acb4cc550f91b6c2a88f59f97edcfc
Thirukumaran V <thiru.vaithiya@gmail.com>
2024-10-24 13:30:58 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,3 +1,12 @@
+resource "aws_kms_key" "my_kms_key" {
+  description = "KMS Key for encryption"
+  key_usage   = "ENCRYPT_DECRYPT"
+
+  tags = {
+    Name = "snyk-key"
+  }
+}
+
 resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   cloud_watch_logs_group_arn = "appLogs"
@@ -5,5 +14,5 @@ resource "aws_cloudtrail" "insecure-logging" {
   enable_logging = true
   enable_log_file_validation = true
   is_multi_region_trail = true
-  kms_key_id = ${env.kmsid}
+  kms_key_id =  aws_kms_key.my_kms_key.id
 }
