99c047feb068a6fba21a3c20fc7d8ea99c2f5bd8
Thirukumaran V <thiru.vaithiya@gmail.com>
2024-10-24 13:41:18 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -3,8 +3,8 @@ resource "aws_s3_bucket" "insecure-bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
-#   block_public_acls       = true
-#   block_public_policy     = true
+   block_public_acls       = true
+   block_public_policy     = true
    ignore_public_acls      = true
    restrict_public_buckets = true
   logging {
