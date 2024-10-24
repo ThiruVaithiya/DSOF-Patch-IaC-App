b2d6834d9dd2c4e1988c29e8460446348758cc86
Thirukumaran V <thiru.vaithiya@gmail.com>
2024-10-24 13:27:01 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -4,4 +4,6 @@ resource "aws_cloudtrail" "insecure-logging" {
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
   enable_log_file_validation = true
+  is_multi_region_trail = true
+  kms_key_id = ${env.kmsid}
 }
