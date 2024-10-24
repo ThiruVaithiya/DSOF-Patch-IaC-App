6a6889ed0b87ca367157361e61dedc9fbb67ba97
Thirukumaran V <thiru.vaithiya@gmail.com>
2024-10-24 13:40:33 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -5,23 +5,15 @@ resource "aws_s3_bucket" "insecure-bucket" {
 resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
 #   block_public_acls       = true
 #   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
+   ignore_public_acls      = true
+   restrict_public_buckets = true
   logging {
     target_bucket = aws_s3_bucket.insecure-bucket.id  # Bucket where logs will be stored
     target_prefix = "log/"
   }
   versioning {
     enabled = true
-  }
-}
-
-# Add versioning to the S3 bucket
-resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
-  bucket = aws_s3_bucket_public_access_block.my_bucket.bucket
-  versioning_configuration {
-    status = "Enabled"
-    mfa_delete = "Enabled"
+    mfa_delete = true
   }
 }
 
