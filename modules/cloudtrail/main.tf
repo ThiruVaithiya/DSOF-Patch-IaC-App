c40de8e31f2b0708dabca581c33de9c83db7e482
Thirukumaran V <thiru.vaithiya@gmail.com>
2024-10-24 13:25:11 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
+  cloud_watch_logs_group_arn = "appLogs"
   s3_bucket_name = "my-cloudtrail-bucket"
-  enable_logging = false
+  enable_logging = true
   enable_log_file_validation = true
 }
