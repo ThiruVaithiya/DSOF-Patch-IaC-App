635246c58bfd8bf1bb6c0fc9509c6d03f2073fb1
Thirukumaran V <thiru.vaithiya@gmail.com>
2024-10-24 14:05:37 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,17 +2,19 @@ resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
 }
 
+resource "aws_s3_bucket" "logging-bucket" {
+  bucket = "logging-bucket"
+}
+
 resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
    block_public_acls       = true
    block_public_policy     = true
    ignore_public_acls      = true
    restrict_public_buckets = true
-
    logging {
-    target_bucket = aws_s3_bucket.insecure-bucket.id
+    target_bucket = aws_s3_bucket.logging-bucket.id
     target_prefix = "log/"
   }
-
 }
 
 resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
@@ -24,28 +26,6 @@ resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
     mfa_delete = true
   }
 }
-
-# Create a bucket to store logs
-resource "aws_s3_bucket" "my_log_bucket" {
-  bucket = "my-log-bucket"
-}
-
-# Create the main S3 bucket with logging enabled
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = aws_s3_bucket.insecure-bucket.id
-
-  # Enable logging
-  logging {
-    target_bucket = aws_s3_bucket.my_log_bucket.id  # Bucket where logs will be stored
-    target_prefix = "log/"  # Prefix for the log files
-  }
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
-}
-
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
